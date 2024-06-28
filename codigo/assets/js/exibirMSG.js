3d706dab58cdd7c17c199f862a0c355f883f1850
Matheus de Almeida <69125506+matheusmra@users.noreply.github.com>
2024-06-28 12:15:55 -0300

    tetst

--- codigo/assets/js/exibirMSG.js
+++ codigo/assets/js/exibirMSG.js
@@ -1,24 +1,19 @@
 const divMensagens = document.querySelector("#comentarios");
-const receitaSelecionada = 1; //Caso queira testar a exibição, altere esse valor para outro número - Essa parte depende da sprint 3 de outro integrante
-//Não se esqueça de alterar o id da receita no db.json
+
 fetch("/assets/db/db.json").then((response) => {
     response.json().then((dados) => {
-        const mensagensFiltradas = dados.comentarios.filter((comentario) => {
-            return comentario.receitaID === receitaSelecionada;
-        });
-
-        mensagensFiltradas.forEach((comentario) => {
+    
             const itemLista = document.createElement("li");
             itemLista.classList.add("mensagem-comentario");
             
             const nomeSpan = document.createElement("span");
-            nomeSpan.textContent = comentario.nome;
+            nomeSpan.textContent = comentarios.nome;
             nomeSpan.style.fontWeight = "bold"; 
             
             itemLista.appendChild(nomeSpan);
-            itemLista.innerHTML += `: ${comentario.texto}`;
+            itemLista.innerHTML += `: ${comentarios.texto}`;
             
             divMensagens.appendChild(itemLista);
-        });
+        
     }); 
 });
